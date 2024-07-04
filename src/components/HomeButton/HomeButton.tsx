94835625c9380bd276a6e6d1bd165dd488772b08
mariabdma <mariabma76@gmail.com>
2024-07-04 11:05:44 -0300

    [Feature] Creation of Homepage

---
+++ src/components/HomeButton/HomeButton.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { ButtonContainer, ButtonText } from "./styles";
+
+const HomeButton: React.FC = () => {
+  return (
+    <ButtonContainer>
+      <ButtonText>Faça um agendamento para a vacina COVID-19</ButtonText>
+    </ButtonContainer>
+  );
+};
+
+export default HomeButton;
